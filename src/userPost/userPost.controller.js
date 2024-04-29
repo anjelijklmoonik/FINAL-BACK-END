0e48033ac09be6591324221a3401f6766df317a4
Melrick1 <melrickbatuwael@gmail.com>
2024-04-29 23:07:58 +0800

    Added UserPostings

---
+++ src/userPost/userPost.controller.js
@@ -0,0 +1,48 @@
+const postinganService = require ('./userPost.service')
+
+const getAllPostingan = async (req, res) => {
+    try {
+        const data = await postinganService.getAllPostingan ()
+        res.status(200).json({
+            status: 'success',
+            data,
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500).send('Internal Server Error')
+    }
+};
+
+const postPostingan = async (req, res) => {
+    const {name, address, phoneNumber} = req.body;
+    try {
+        const data = await postinganService.postPostingan (name, address, phoneNumber)
+        res.status(200).json({
+            status: 'success',
+            data,
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500).send('Internal Server Error')
+    }
+};
+
+const getPostByID = async (req, res) => {
+    const {id} = req.params;
+    try {
+        const data = await postinganService.getPostByID (id)
+        res.status(200).json({
+            status: 'success',
+            data,
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500).send('Internal Server Error')
+    }
+};
+
+module.exports = {
+    getAllPostingan,
+    postPostingan,
+    getPostByID,
+}
---
+++ src/userPost/userPost.repository.js
@@ -0,0 +1,34 @@
+const prisma = require ('../db');
+
+const getAllPostingan = async () => {
+    return await prisma.postingan.findMany();
+}
+
+const postPostingan = async (name, photo, caption, location, phoneNumber, status) => {
+    return await prisma.postingan.create({
+        data: {
+          name,
+          photo,
+          caption,
+          location,
+          phoneNumber,
+          status,
+        },
+      });
+}
+
+const getPostByID = async (id) => {
+    return await prisma.postingan.findUnique({
+        where: {
+          id
+        },
+      });
+}
+
+module.exports = {
+  getAllPostingan,
+  postPostingan,
+  getPostByID,
+    // updateStudent,
+    // deleteStudent
+}
