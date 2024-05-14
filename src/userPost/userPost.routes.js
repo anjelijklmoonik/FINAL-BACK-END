93e3d2c6ec8761f014d6048d120660628a16c5a7
christinaaa77 <114158922+christinaaa77@users.noreply.github.com>
2024-05-14 11:05:41 +0800

    Update userPost.routes.js

--- src/userPost/userPost.routes.js
+++ src/userPost/userPost.routes.js
@@ -1,10 +1,11 @@
-const express = require('express')
-const router = express.Router()
-const userPostController = require('./userPost.controller')
+const express = require('express');
+const router = express.Router();
+const userPostController = require('./userPost.controller');
 
-router.get('/postingan', userPostController.getAllPostingan)
-router.get('/postingan/:id', userPostController.getPostByID)
-router.post('/postingan', userPostController.postPostingan)
+router.get('/postingan', userPostController.getAllPostingan);
+router.get('/postingan/:id', userPostController.getPostByID);
+router.post('/postingan', userPostController.postPostingan);
+router.put('/postingan/:id', userPostController.updatePost);
+router.delete('/postingan/:id', userPostController.deletePost);
 
-
-module.exports = router
+module.exports = router;
