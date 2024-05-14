9829da304d0541415fff998cf8fc79b0729a3ee7
christinaaa77 <114158922+christinaaa77@users.noreply.github.com>
2024-05-14 11:06:15 +0800

    Update userPost.service.js

--- src/userPost/userPost.service.js
+++ src/userPost/userPost.service.js
@@ -1,22 +1,32 @@
-const postinganRepository = require ('./userPost.repository')
+const postinganRepository = require('./userPost.repository');
 
 const getAllPostingan = async () => {
-    return await postinganRepository.getAllPostingan()
+    return await postinganRepository.getAllPostingan();
 }
 
 const postPostingan = async (name, caption, location, phoneNumber, status) => {
     if (!name || !caption || !location || !phoneNumber || !status) {
-        throw new Error ('Data tidak lengkap')
+        throw new Error('Data tidak lengkap');
     }
-    return await postinganRepository.postPostingan(name, caption, location, phoneNumber, status)
+    return await postinganRepository.postPostingan(name, caption, location, phoneNumber, status);
 }
 
 const getPostByID = async (id) => {
-    return await postinganRepository.getPostByID(id)
+    return await postinganRepository.getPostByID(id);
+}
+
+const updatePost = async (id, data) => {
+    return await postinganRepository.updatePost(id, data);
+}
+
+const deletePost = async (id) => {
+    return await postinganRepository.deletePost(id);
 }
 
 module.exports = {
     getAllPostingan,
     postPostingan,
     getPostByID,
+    updatePost,
+    deletePost,
 }
